434e09378ec5eb73918648073d7f7c88ea4b3d97
Sam <sam333lewis@gmail.com>
2021-08-17 21:22:34 +0100

    fixed a bug from commit `563b6f4`

--- helpers.py
+++ helpers.py
@@ -8,7 +8,7 @@ from pathlib import Path
 def file_size(file_path: str) -> int:
     if os.path.isfile(file_path):
         file_info = os.stat(file_path)
-        return file_info.st_size / 1000000
+        return round(file_info.st_size / 1000000)
     return 0
 
 
