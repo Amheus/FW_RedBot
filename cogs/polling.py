28c38a5369312c1c9975bee7b41c2b3b9d4db448
Sam <sam333lewis@gmail.com>
2021-08-17 22:56:30 +0100

    remind me to come back to this lol

--- cogs/polling.py
+++ cogs/polling.py
@@ -19,13 +19,13 @@ class Polling(commands.Cog):
 
         msg = context.message
 
-        messageContent = msg.clean_content
+        message_content = msg.clean_content
 
         # --------------------------------------------------
         # NO PARAMETERS
         # --------------------------------------------------
 
-        if messageContent.find("{") == -1:
+        if message_content.find("{") == -1:
             await context.message.add_reaction(u"\U0001F44D")
             await context.message.add_reaction(u"\U0001F44E")
             await context.message.add_reaction(u"\U0001F937")
@@ -35,35 +35,32 @@ class Polling(commands.Cog):
         # CUSTOM OPTIONS
         # --------------------------------------------------
 
-        first = messageContent.find("{") + 1
-        second = messageContent.find("}")
-        title = messageContent[first:second]
+        title = message_content[message_content.find("{") + 1:message_content.find("}")]
 
         # gets the number of options and assigns them to an array
-        newMessage = messageContent[second:]
-        loopTime = 0
+        new_message = message_content[message_content.find("}"):]
+        loop_time = 0
 
         option = []
-        for options in messageContent:
-            # get from } [option 1]
-            stillOptions = newMessage.find("[")
-            if stillOptions != -1:
-                if loopTime == 0:
-                    first = newMessage.find("[") + 1
-                    second = newMessage.find("]")
+        for _ in message_content:
+            still_options = new_message.find("[")
+            if still_options != -1:
+                if loop_time == 0:
+                    first = new_message.find("[") + 1
+                    second = new_message.find("]")
                     second1 = second + 1
-                    option.append(newMessage[first:second])
-                    loopTime += 1
+                    option.append(new_message[first:second])
+                    loop_time += 1
                 else:
-                    newMessage = newMessage[second1:]
-                    first = newMessage.find("[") + 1
-                    second = newMessage.find("]")
+                    new_message = new_message[second1:]
+                    first = new_message.find("[") + 1
+                    second = new_message.find("]")
                     second1 = second + 1
-                    option.append(newMessage[first:second])
-                    loopTime += 1
+                    option.append(new_message[first:second])
+                    loop_time += 1
 
         try:
-            pollMessage = ""
+            poll_message = ""
             i = 0
             for choice in option:
                 if not option[i] == "":
@@ -72,23 +69,23 @@ class Polling(commands.Cog):
                         log_event(level=ERROR, details="Command Failed, Too many Options")
                         return
                     elif not i == len(option) - 1:
-                        pollMessage = pollMessage + "\n\n" + emoji_letters[i] + " " + choice
+                        poll_message = poll_message + "\n\n" + emoji_letters[i] + " " + choice
                 i += 1
 
             e = discord.Embed(
                 title="**" + title + "**",
-                description=pollMessage,
+                description=poll_message,
                 colour=0x83bae3
             )
-            pollMessage = await msg.channel.send(embed=e)
+            poll_message = await msg.channel.send(embed=e)
             i = 0
             final_options = []
             for choice in option:
                 if not i == len(option) - 1 and not option[i] == "":
                     final_options.append(choice)
-                    await pollMessage.add_reaction(emoji_letters[i])
+                    await poll_message.add_reaction(emoji_letters[i])
                 i += 1
-            log_event(level=DEBUG, details="Command Succesfull")
+            log_event(level=DEBUG, details="Command successfully executed")
         except Exception as error:
             log_event(level=ERROR, details="Command Failed, Incorrect Format [{error}]")
             await msg.channel.send(
